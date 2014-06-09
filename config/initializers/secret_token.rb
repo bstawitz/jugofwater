314d3a8f257c8a661a5b430f8e77a99d857b95d8
bstawitz <stawitzb@gmail.com>
2014-06-09 03:03:54 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dothedue::Application.config.secret_token = '70e05554a29455d801e590cbd1ac265bb89bf0593bf41774625f4a8ad741db6a9c17ed93f61902d060954906f0299b4511bcebb8bf8389dd74f3a05db682c5de'
